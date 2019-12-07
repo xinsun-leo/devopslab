35dd57f9b886255df7d4085b585da915c3cbd768
xinsun-leo <45778006+xinsun-leo@users.noreply.github.com>
2019-12-07 13:41:01 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,7 +1,7 @@
 export GITACCOUNT=xinsun
 
 export MYCLUSTER=bnkk3bst05dokf3mnma0
-export KUBECONFIG=
+export KUBECONFIG=/tmp/ic/AA/.bluemix/plugins/container-service/clusters/bnkk3bst05dokf3mnma0/kube-config-tok04-tektonknativebeijing04.yml
 
 export NAMESPACE=xinsun-knative
 export EMAIL=xinsun_work@sina.com
